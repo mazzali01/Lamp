ccbac9d472d6817169b507dfce7a8e7d9344fd5a
mazzali01 <132758532+mazzali01@users.noreply.github.com>
2024-02-24 19:29:29 -0300

    Add files via upload
    atualizacao para html

--- index.js
+++ index.js
@@ -22,42 +22,28 @@ const logo = document.querySelector('#imgFundo')
 const html = document.querySelector('html')
 const lamp = document.querySelector('#dm')
 
-lamp.addEventListener('click', function() {
-    html.classList.toggle('dark-mode');
-    if (html.classList.contains('dark-mode')) {
-        logo.setAttribute('src', 'assets/lamp2Bsemfundo.png');
-        console.log("teste");
-    } else {
-        logo.setAttribute('src', 'assets/lamp2semfundo.png');
-        console.log("teste");
-    }
-
-
-
-
-});
 
 const missao = document.getElementById('missao')
 const visao = document.getElementById('visao')
 const valores = document.getElementById('valores')
-const mvv = document.getElementById('container-mvv')
+const mvv = document.getElementById('mvv')
 
-var missaotxt = '<p> proporcionar a todos os alunos e aprendizes uma educação acessível e de alta qualidade. Isso inclui o fornecimento de recursos educacionais, ferramentas interativas e suporte personalizado para ajudar os alunos a atingirem seu potencial máximo. A missão também se concentra em promover a igualdade de oportunidades educacionais, capacitando os alunos a alcançar o sucesso acadêmico e profissional, independentemente de onde estejam localizados ou de seus antecedentes.</p>';
-var visaotxt = '<p>A visão de uma plataforma acadêmica de estudos é criar um mundo em que a educação seja verdadeiramente global, personalizada e eficaz. Isso envolve o desenvolvimento contínuo de tecnologias inovadoras e métodos de ensino que se adaptem às necessidades individuais dos alunos. A visão busca inspirar a curiosidade, o pensamento crítico e a busca pelo conhecimento, capacitando os alunos a se tornarem cidadãos do mundo bem-informados, criativos e resilientes.</p>';
-var valorestxt = '<p>Nossos valores incluem a busca constante pela excelência educacional, o compromisso com a acessibilidade, a ênfase na inovação, a promoção da diversidade e inclusão, a responsabilidade social e o apoio ao desenvolvimento pessoal e profissional dos alunos. Esses valores moldam a cultura da plataforma e garantem que ela cumpra sua missão de oferecer educação de qualidade, acessível e enriquecedora, capacitando os alunos a prosperarem em seu percurso educacional e profissional.<p>'
+var missaotxt = '<p id=missaotxt>Nossa missão é proporcionar a todos os alunos e aprendizes uma educação acessível e de alta qualidade. Isso inclui o fornecimento de recursos educacionais, ferramentas interativas e suporte personalizado para ajudar os alunos a atingirem seu potencial máximo. A missão também se concentra em promover a igualdade de oportunidades educacionais, capacitando os alunos a alcançar o sucesso acadêmico e profissional, independentemente de onde estejam localizados ou de seus antecedentes.</p>';
+var visaotxt = '<p id=visaotxt>A visão de uma plataforma acadêmica de estudos é criar um mundo em que a educação seja verdadeiramente global, personalizada e eficaz. Isso envolve o desenvolvimento contínuo de tecnologias inovadoras e métodos de ensino que se adaptem às necessidades individuais dos alunos. A visão busca inspirar a curiosidade, o pensamento crítico e a busca pelo conhecimento, capacitando os alunos a se tornarem cidadãos do mundo bem-informados, criativos e resilientes.</p>';
+var valorestxt = '<p id=valorestxt>Nossos valores incluem a busca constante pela excelência educacional, o compromisso com a acessibilidade, a ênfase na inovação, a promoção da diversidade e inclusão, a responsabilidade social e o apoio ao desenvolvimento pessoal e profissional dos alunos. Esses valores moldam a cultura da plataforma e garantem que ela cumpra sua missão de oferecer educação de qualidade, acessível e enriquecedora, capacitando os alunos a prosperarem em seu percurso educacional e profissional.<p>'
 var mvvtxt = '';
 
 missao.addEventListener('click', function() {
-    document.getElementById('container-mvv').innerHTML = missaotxt;
+    document.getElementById('mvv').innerHTML = missaotxt;
     console.log("TESTE");
 });
 
 visao.addEventListener('click', function() {
-    document.getElementById('container-mvv').innerHTML = visaotxt;
+    document.getElementById('mvv').innerHTML = visaotxt;
     console.log("TESTE");
 });
 
 valores.addEventListener('click', function() {
-    document.getElementById('container-mvv').innerHTML = valorestxt;
+    document.getElementById('mvv').innerHTML = valorestxt;
     console.log("TESTE");
 });
