2e68f28ba9e519d9f748e2c9c273ec752ecf021c
userlocal <vinitrinda5005@gmail.com>
2024-03-14 14:36:11 -0300

    Atualiza certos erros do import.bd

--- import.bd.js
+++ import.bd.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, set, get, child} from "https://www.gstatic.com/fireba
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBfJuEDQ7KIPO_feHPmtY4DXRPP-ZqpmVY",
-  authDomain: "lamp-7fb6e.firebaseapp.com",
-  databaseURL: "https://lamp-7fb6e-default-rtdb.firebaseio.com",
-  projectId: "lamp-7fb6e",
-  storageBucket: "lamp-7fb6e.appspot.com",
-  messagingSenderId: "816320075310",
-  appId: "1:816320075310:web:2ba343469b2ade2886f1bf"
+apiKey: "AIzaSyBfJuEDQ7KIPO_feHPmtY4DXRPP-ZqpmVY",
+authDomain: "lamp-7fb6e.firebaseapp.com",
+databaseURL: "https://lamp-7fb6e-default-rtdb.firebaseio.com",
+projectId: "lamp-7fb6e",
+storageBucket: "lamp-7fb6e.appspot.com",
+messagingSenderId: "816320075310",
+appId: "1:816320075310:web:2ba343469b2ade2886f1bf"
 };
 
 // Initialize Firebase
@@ -25,15 +25,20 @@ const dataNasc = document.getElementById('nasc');
 const senha = document.getElementById('senha');
 
 btnCadastro.addEventListener('click', function(e){
-
-  set(ref(bd, 'user/' + apelido.value),{
-      Apelido: apelido.value,
-      Nome: nome.value,
-      Email: email.value,
-      Telefone: tel.value,
-      Nascimento: dataNasc.value, 
-      Senha: senha.value
-  });
-  alert("FUNCIONOU PORRA")
+    e.preventDefault();
+    set(ref(bd, 'Users/' + apelido.value),{
+        Apelido: apelido.value,
+        Nome: nome.value,
+        Email: email.value,
+        Telefone: tel.value,
+        Nascimento: dataNasc.value, 
+        Senha: senha.value
+    }).then(()=>{
+      alert("O formulário foi enviado com sucesso!");
+    })
+    .catch(() =>{
+      alert("Erro ao enviar formulário");
+    });
 });
-
+ ;
+    
